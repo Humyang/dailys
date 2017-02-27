51f445ab037bea802b9662855b97efe0ae4a3549
Yangxg <jessebutchman@gmail.com>
2017-02-27 23:55:11 +0800

    更新

--- src/components/WriteArticle.vue
+++ src/components/WriteArticle.vue
@@ -1,11 +1,24 @@
 <template>
-    <textarea name="" id="" cols="30" rows="10">
+    <div class="write_article_wrap">
+      <div class="side_view">
+        <p><a href="#">asdasdasdasdasd</a></p>
+        <p><a href="#">asdasdasdasdasd</a></p>
+        <p><a href="#">asdasdasdasdasd</a></p>
+        <p><a href="#">asdasdasdasdasd</a></p>
+        <p><a href="#">asdasdasdasdasd</a></p>
+      </div>
+      <div class="content">
+        <textarea name="" id="" cols="30" rows="10">
         
-    </textarea>
+        </textarea>
+      </div>
+    </div>
+    
 </template>
 
 <script>
 import { mapState,mapGetters,mapMutations,mapActions } from 'vuex'
+import '../css/WriteArticle.css'
 export default {
   data () {
     return {
