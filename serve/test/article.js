efa6c9202646bce3cd593b93ce49c29659e7a983
Yangxg <jessebutchman@gmail.com>
2017-03-06 17:49:21 +0800

    更新

--- serve/test/article.js
+++ serve/test/article.js
@@ -4,15 +4,11 @@ var CODE = require('../PREDEFINED/ERROR_CODE.js').CODE
 describe('单词正删改查模块----', function() {
     it('增删改查', function(done) {
         co(function*(){
-            let verifycode = yield API.verify_code()
-            assert(verifycode.status,true,verifycode)
+            let article_update = yield API.articleUpdate('111','222')
+            assert(article_update.status,true,article_update)
+            done()
         }).catch(function(err){
-            if(err.STATUSCODE === CODE.WORD_NOT_FIND.STATUSCODE){
-                done()
-            }else{
-                console.log(err)
-                done(err)
-            }
+            done(err)
         })
     })
 })
