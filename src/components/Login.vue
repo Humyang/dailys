7a6dc4db8125aadecc8b371ed99b9c1cbc341975
Yangxg <jessebutchman@gmail.com>
2017-03-15 00:29:21 +0800

    登陆页面

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="login_wrap">
+		<p><input placeholder="账号" type="" name=""></p>
+		<p><input placeholder="密码" type="" name=""></p>
+		<div class="btn_wrap">
+			<a href="#" class="btn black" >登陆</a>
+			<p class="p1"><a href="">直接注册并登陆</a></p>
+		</div>
+	</div>
+</template>
+<script >
+	import '../css/comman.css'
+	import '../css/btn.css'
+	import '../css/Login.css'
+	export default {
+  		data () {
+    		return {}
+    	},
+    	method:{
+
+    	},
+    	computed: {
+    
+  		}
+	}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '../components/Hello'
 import WriteArticle from '../components/WriteArticle'
+import Login from '../components/Login'
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
@@ -15,6 +15,11 @@ export default new Router({
         path:'/WriteArticle',
         name:'WriteArticle',
         component:WriteArticle
+    },
+    {   
+        path:'/Login',
+        name:'Login',
+        component:Login
     }
   ]
 })
