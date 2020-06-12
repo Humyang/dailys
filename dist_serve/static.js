a8f35cf00b6ae346ec47f64b29ae061140c01390
杨雄贵 <378029633@qq.com>
2020-06-12 11:21:34 +0800

    1

--- dist_serve/static.js
+++ dist_serve/static.js
@@ -3,6 +3,7 @@ var app = new koa();
 var serve = require('koa-static');
 const path = require('path');
 const render = require('koa-ejs');
+const compress = require('koa-compress')
 render(app, {
     root: path.join(__dirname+'./../dist'),
     layout: false,
@@ -15,14 +16,27 @@ var koaRouter = require('koa-router')
 const router = new koaRouter();
 router.get('*',async function (ctx,next) {
     // debugger
-    
+    ctx.compress = true
     if(ctx.URL.pathname.indexOf('.')!="-1"){
         await next()
     }else{
         await ctx.render('index')
     }
     
-},serve(__dirname+'./../dist',{maxage:3153600000}))
+},serve(__dirname+'./../dist',{maxage:3153600000,gzip:true}))
+app.use(compress({
+    filter (content_type) {
+        return /text/i.test(content_type)
+    },
+    threshold: 2048,
+    gzip: {
+      flush: require('zlib').Z_SYNC_FLUSH
+    },
+    deflate: {
+      flush: require('zlib').Z_SYNC_FLUSH,
+    },
+    br: false // disable brotli
+  }))
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(8200);
